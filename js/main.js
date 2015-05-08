f9f5ecf45f884996701baf09a3c0109ac9c93591
Wayne Yang <waysidekoi@gmail.com>
2015-05-08 01:03:02 -0400

    veritcal timeline

---
+++ js/main.js
@@ -0,0 +1,15 @@
+<script>
+	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  	ga('create', 'UA-48014931-1', 'codyhouse.co');
+  	ga('send', 'pageview');
+
+  	jQuery(document).ready(function($){
+  		$('.close-carbon-adv').on('click', function(){
+  			$('#carbonads-container').hide();
+  		});
+  	});
+</script>
